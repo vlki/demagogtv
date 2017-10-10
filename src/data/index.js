688617d5ded620e697c4bcb46d5d3be822e1ea33
Jan Vlcek <vlki@vlki.cz>
2017-10-10 18:16:39 +0200

    Sorted by latest survey

--- src/data/index.js
+++ src/data/index.js
@@ -12,21 +12,41 @@ import robejsek from './robejsek'
 import stropnicky from './stropnicky'
 import zaoralek from './zaoralek'
 
+// sorted by survey at
+// http://www.ceskatelevize.cz/ct24/2268226-volebni-potencial-ano-klesl-na-325-procenta-pirati-a-spd-posilili
 export const DEBATES_LIST = [
-  cech, // 6. rijna
-  farsky, // 5. rijna
-  robejsek, // 1. rijna
-  okamura, // 1. rijna
-  bartos, // 29. zari
-  kalousek, // 28. zari
-  mach, // 27. zari
-  belobradek, // 24. zari
-  stropnicky, // 20. zari
-  zaoralek, // 15. zari
-  fiala, // 12. zari
-  babis, // 10. zari
-  filip // 8. zari
+  babis,
+  zaoralek,
+  fiala,
+  okamura,
+  bartos,
+  filip,
+  kalousek,
+  farsky,
+  belobradek,
+  stropnicky,
+
+  // these are not in the survey, so just sorted randomly
+  mach,
+  robejsek,
+  cech
 ]
+
+// export const DEBATES_LIST = [
+//   cech, // 6. rijna
+//   farsky, // 5. rijna
+//   robejsek, // 1. rijna
+//   okamura, // 1. rijna
+//   bartos, // 29. zari
+//   kalousek, // 28. zari
+//   mach, // 27. zari
+//   belobradek, // 24. zari
+//   stropnicky, // 20. zari
+//   zaoralek, // 15. zari
+//   fiala, // 12. zari
+//   babis, // 10. zari
+//   filip // 8. zari
+// ]
 
 export const DEBATES_BY_PATH = DEBATES_LIST.reduce(
   (carry, debate) => Object.assign(carry, {
