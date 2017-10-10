5cebd73bd073b750b66151af50d98d64402597f6
Jan Vlcek <vlki@vlki.cz>
2017-10-10 12:23:45 +0200

    Added SPO

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,11 @@
 import babis from './babis'
 import bartos from './bartos'
 import belobradek from './belobradek'
+import cech from './cech'
 import mach from './mach'
 
 export const DEBATES_LIST = [
+  cech,
   bartos,
   mach,
   belobradek,
