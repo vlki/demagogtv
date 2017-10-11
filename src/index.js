5ff1911a54683167360505f532c3dcb474fcb531
Jan Vlcek <vlki@vlki.cz>
2017-10-11 20:09:43 +0200

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from 'react-ga'
+import PropTypes from 'prop-types'
 import smoothscrollPolyfill from 'smoothscroll-polyfill'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,9 +11,33 @@ import App from './App'
 
 smoothscrollPolyfill.polyfill()
 
+ReactGA.initialize('UA-107985925-1')
+
+class GAListener extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  componentDidMount() {
+    this.sendPageView(this.context.router.history.location)
+    this.context.router.history.listen(this.sendPageView)
+  }
+
+  sendPageView(location) {
+    ReactGA.set({ page: location.pathname })
+    ReactGA.pageview(location.pathname)
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
 ReactDOM.hydrate(
   <BrowserRouter>
-    <App />
+    <GAListener>
+      <App />
+    </GAListener>
   </BrowserRouter>,
   document.getElementById('root')
 )
