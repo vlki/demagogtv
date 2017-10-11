c38cea87620001da86aa1ea8a48d338698db8d67
Jan Vlcek <vlki@vlki.cz>
2017-10-11 21:33:20 +0200

    Status 404 for not found page

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>Page not found</h1>
-        <Link to="/">Go to home</Link>
-      </div>
+      <RouteStatus statusCode={404}>
+        <Container className="container-fluid">
+          <h1>Stránka neexistuje</h1>
+          <Link to="/">Zpět na úvod DemagogTV</Link>
+        </Container>
+      </RouteStatus>
     )
   }
 }
+
+const Container = styled.div`
+  max-width: 1000px;
+  padding-top: 35px;
+`
+
+// From react-router example
+// https://reacttraining.com/react-router/web/example/static-router
+const RouteStatus = (props) => (
+  <Route
+    render={({ staticContext }) => {
+      // we have to check if staticContext exists
+      // because it will be undefined if rendered through a BrowserRouter
+      if (staticContext) {
+        staticContext.statusCode = props.statusCode
+      }
+
+      return (
+        <div>
+          {props.children}
+        </div>
+      )
+    }}
+  />
+)
 
 export default PageNotFound
