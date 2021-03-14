c271ef9af94a872e13f7009d23e7a922e722ee75
Jan Vlcek <vlki@vlki.cz>
2021-03-14 11:46:23 +0100

    Use plausible instead of google analytics

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import ReactGA from 'react-ga'
-import PropTypes from 'prop-types'
-import smoothscrollPolyfill from 'smoothscroll-polyfill'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import smoothscrollPolyfill from "smoothscroll-polyfill";
 
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
-import App from './App'
+import App from "./App";
 
-smoothscrollPolyfill.polyfill()
-
-ReactGA.initialize('UA-107985925-1', { debug: true })
-
-class GAListener extends React.Component {
-  static contextTypes = {
-    router: PropTypes.object
-  }
-
-  componentDidMount() {
-    this.sendPageView(this.context.router.history.location)
-    this.context.router.history.listen(this.sendPageView)
-  }
-
-  sendPageView(location) {
-    ReactGA.set({ page: location.pathname })
-    ReactGA.pageview(location.pathname)
-  }
-
-  render() {
-    return this.props.children
-  }
-}
+smoothscrollPolyfill.polyfill();
 
 ReactDOM.hydrate(
   <BrowserRouter>
-    <GAListener>
-      <App />
-    </GAListener>
+    <App />
   </BrowserRouter>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
